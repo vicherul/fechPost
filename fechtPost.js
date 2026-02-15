977217353ecbb12a7c621c91446420a55a87933a
Victor Hernandez <239040990+vitorioh-blip@users.noreply.github.com>
2026-02-15 21:15:35 +0100

    primer commit

---
+++ fechtPost.js
@@ -0,0 +1,28 @@
+async function searchPost(id) {
+  try {
+    // 1. Construir la URL con Template Literals
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    // 2. Hacer la petición al servidor
+    const response = await fetch(url);
+
+    // 3. Lógica de validación: 
+    // Si el post no existe (error 404), lanzamos un error manualmente 
+    // para que el código salte al bloque 'catch'.
+    if (!response.ok) {
+      throw new Error("ID no encontrado");
+    }
+
+    // 4. Convertir la respuesta a formato JSON (objeto de JS)
+    const post = await response.json();
+
+    // 5. Imprimir el título en MAYÚSCULAS como pide el ejercicio
+    console.log(post.title.toUpperCase());
+
+  } catch (error) {
+    // 6. Si algo falló arriba, mostramos el mensaje solicitado
+    console.log("El post no pudo ser hallado");
+  }
+}
+
+// Ejemplo de uso:
+searchPost(1);  // Imprimirá el título en mayúsculas
